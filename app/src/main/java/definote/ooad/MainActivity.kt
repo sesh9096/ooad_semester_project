cb0825eb658a8c768a7a2102232e427f682accef
JackPaulman <japa9577@colorado.edu>
2024-04-12 14:43:40 -0600

    Added search bar

--- app/src/main/java/definote/ooad/MainActivity.kt
+++ app/src/main/java/definote/ooad/MainActivity.kt
@@ -3,11 +3,13 @@ package definote.ooad
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
+import androidx.compose.material3.TextField
 import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import definote.ooad.ui.theme.MyApplicationTheme
@@ -17,9 +19,15 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             MyApplicationTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
-                    Greeting("Android")
+                Surface(modifier = Modifier.fillMaxWidth(), color = MaterialTheme.colorScheme.background) {
+                    var searchText by remember { mutableStateOf("") }
+                    Column {
+                        SearchBar(
+                            searchText = searchText,
+                            onSearchTextChanged = { searchText = it }
+                        )
+                        Greeting(name = "Android", searchText = searchText)
+                    }
                 }
             }
         }
@@ -27,17 +35,27 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-            text = "Hello $name!",
-            modifier = modifier
+fun SearchBar(searchText: String, onSearchTextChanged: (String) -> Unit) {
+    TextField(
+        value = searchText,
+        onValueChange = onSearchTextChanged,
+        label = { Text("Search") }
     )
+}
+
+@Composable
+fun Greeting(name: String, searchText: String) {
+    Text(text = "Hello $name! Searching for: $searchText")
 }
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     MyApplicationTheme {
-        Greeting("Jack")
+        Column {
+            val searchText = remember { mutableStateOf("World") }
+            SearchBar(searchText = searchText.value) { searchText.value = it }
+            Greeting(name = "Android", searchText = searchText.value)
+        }
     }
 }
